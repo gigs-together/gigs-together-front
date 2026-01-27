3f6279514e2a2d090a766530f9276d19a766dee5
Arina M <thearinamarr@gmail.com>
2026-01-27 20:05:56 +0100

    fix: accelerate pre-commit actions

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,6 +3,9 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  '*.{js,ts,jsx,tsx}': ['prettier --write', 'eslint --fix'],
-  '*.{json,md,css,scss,yml,yaml}': ['prettier --write'],
+  '*.{js,ts,jsx,tsx}': [
+    'prettier --cache --cache-location .cache/prettier --write',
+    'eslint --cache --cache-location .cache/eslint --fix',
+  ],
+  '*.{json,md,css,scss,yml,yaml}': ['prettier --cache --cache-location .cache/prettier --write'],
 };
